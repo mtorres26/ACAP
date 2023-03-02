5028c93ffa3b04af2839488adf5267e0deb1775a
mtorres26 <migueltorresalonso1@gmail.com>
2023-03-02 18:09:47 +0100

    añadiendo lo primero

--- 2022_23-SeminarioMPI/ejemplo4.c
+++ 2022_23-SeminarioMPI/ejemplo4.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <mpi.h>
 
+const int tam = 10;
+
 int main(int argc, char* argv[])
 {
-    int rank, size;
+    int rank, size, v[tam];
     MPI_Status status;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if(size != 2)
+    if(size < 2)
     {
         if(rank==0)
         {
@@ -20,7 +22,18 @@ int main(int argc, char* argv[])
     {
         if(rank == 0)
         {
+            for(int i = 0; i < tam-1; i++)
+            {
+                v[i] = i;
+                MPI_Send(v, tam, MPI_INT, i%size, 0, MPI_COMM_WORLD);
+            }
+        }
+        if(rank != MASTER)
+        {
             
-        }    
+        } 
     }
+
+
+    MPI_Finalize();
 }
